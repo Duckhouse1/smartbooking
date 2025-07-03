a2321c81fa1d2e3ca4e31757022bba276a951ee7
Dennis <eaa23ddh@students.eaaa.dk>
2025-07-03 15:00:13 +0200

    paragraph rettelse

--- src/components/Sites/Home.tsx
+++ src/components/Sites/Home.tsx
@@ -2,7 +2,7 @@ export const HomeSection: React.FC = () => {
     return (
         <div>
             <h1>HOME</h1>
-            <p>Beskrivelse</p>
+            <p>Dette er Home page sitet</p>
         </div>
     )
 }
