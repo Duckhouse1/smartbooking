247bb80f7fb16b97a76df9927bb612c8717bec63
Dennis <eaa23ddh@students.eaaa.dk>
2025-07-03 15:07:20 +0200

    Rettet nichlas fejl

--- src/components/Sites/Home.tsx
+++ src/components/Sites/Home.tsx
@@ -2,7 +2,8 @@ export const HomeSection: React.FC = () => {
     return (
         <div>
             <h1>HOME</h1>
-            <p>Dette er Home page sitet</p>
+            <p>Nu det rigtigt</p>
+            <p>Dette er Home page Sited</p>
         </div>
     )
 }
